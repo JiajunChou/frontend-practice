db3342c89c3271fe043d9e6e586018ebef9cbbb7
jiajun.chpu <jiajun.chou@104.com.tw>
2017-06-02 16:29:57 +0800

    Rename exWhatIsJavascript

--- course/Html-Css-Javascript/practice/exWhatIsJavaScripts.js
+++ course/Html-Css-Javascript/practice/exWhatIsJavaScripts.js
@@ -1,24 +1,67 @@
-        var para = document.querySelector('p');
-        para.addEventListener('click', updateName);
+< html >
 
-        function updateName() {
-            var name = prompt('Enter a new name');
-            para.textContent = 'Player 1: ' + name;
-        }
+    <
+    head >
+    <
+    meta charset = "utf-8" >
+    <
+    style >
+    .pStyle {
+        font - family: 'helvetica neue', helvetica, sans - serif;
+        letter - spacing: 1 px;
+        text - transform: uppercase;
+        text - align: center;
+        border: 2 px solid rgba(0, 0, 200, 0.6);
+        background: rgba(0, 0, 200, 0.3);
+        color: rgba(0, 0, 200, 0.6);
+        box - shadow: 1 px 1 px 2 px rgba(0, 0, 200, 0.4);
+        border - radius: 10 px;
+        padding: 3 px 10 px;
+        display: inline - block;
+        cursor: pointer;
+    }
 
-        //Inline JavaScript
+.lastResult {
+    color: white;
+    padding: 3 px;
+} <
+/style> <
+/head>
 
-        var button = document.querySelectorAll('button');
+<
+body >
 
-        function createParagraph() {
-            var para = document.createElement('inline');
-            para.textContent = 'You clicked the button! ';
-            document.body.appendChild(para);
-        }
+    <
+    script >
 
-        var button = document.querySelectorAll('button');
 
-        function removeParagraph() {
-            var para = document.querySelectorAll('inline');
-            para.textContent.remove();
-        }
+
+    var para = document.querySelector('p');
+para.addEventListener('click', updateName);
+
+function updateName() {
+    var name = prompt('Enter a new name');
+    para.textContent = 'Player 1: ' + name;
+}
+
+//Inline JavaScript
+
+var button = document.querySelectorAll('button');
+
+function createParagraph() {
+    var para = document.createElement('inline');
+    para.textContent = 'You clicked the button! ';
+    document.body.appendChild(para);
+}
+
+var button = document.querySelectorAll('button');
+
+function removeParagraph() {
+    var para = document.querySelectorAll('inline');
+    para.textContent.remove();
+} <
+/script> <
+/body>
+
+<
+/html>
