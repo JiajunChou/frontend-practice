3d88451481f71929798e18436856223d8ff0381d
jiajun.chou <jiajun.chou@104.com.tw>
2017-06-03 00:25:38 +0800

    Modify JS-Image_gallery Javascript

--- tutorial/mdn/JS-Image_gallery/main.js
+++ tutorial/mdn/JS-Image_gallery/main.js
@@ -5,10 +5,18 @@ btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+var xxx = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
+var srcImage;
+for (var i = 0; i < 5; i++) {
+    var newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx[i]);
+    thumbBar.appendChild(newImage);
+}
 
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
-  thumbBar.appendChild(newImage);
+function getSrc(elem) {
+    var src = elem.getAttribute('src');
+    return src;
+}
+
 
 /* Wiring up the Darken/Lighten button */
-
